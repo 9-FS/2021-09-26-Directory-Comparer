3766e0c7ef5d59612c7603a6728ae3cff9de7174
9FS <pray4spam@googlemail.com>
2023-04-09 14:03:10 +0000

    init

---
+++ readme.md
@@ -0,0 +1,24 @@
+---
+Topic: "Directory Comparer"
+Author: "구FS"
+---
+<link href="./src/KFS/md_style.css" rel="stylesheet"></link>
+<div id="global">
+
+# <p style="text-align: center">Directory Comparer</p>
+<br>
+<br>
+
+- [1. Overview](#1-overview)
+
+## 1. Overview
+
+Compares given directory's contents and lists the entries that are unique to their respective folders.  
+Limitations:
+
+- It does not use recursion, so only the immediate child entries are compared.
+- It only compares the entry names, not their actual type or content or anything else.
+
+May be useful for manually syncing 2 folders.
+
+</div>
---
+++ src/main.py
@@ -0,0 +1,60 @@
+#Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+import copy
+import KFS.log
+import logging
+import os
+
+
+@KFS.log.timeit
+def main() -> None:
+    dirs_entries: list[list[str]] = []                          #directory 1 and 2 entries all
+    DIRS_LEN: int = 2                                           #number of directories
+    dirs_path: list[str] = []                                   #directory absolute paths
+    dirs_uniques: list[list[str]] = []                          #directory 1 and 2 entries unique
+    result: str = ""                                            #result with all deviations, ready for saving in file
+    RESULT_FILENAME: str = "Directory Differences.txt"          #result file
+    user_input: str                                             #user input, temporary variable
+    
+    i = 0
+    while i<DIRS_LEN:                                           #get user input for all directories
+        logging.info(f"Directory {i+1} path: ")
+        user_input = input()                                    #path input, crash if user is stupid and gives wrong path
+                                                                #user_input="C:/Users/Felix/Dropbox/Musik/Bibliothek"
+        logging.info(user_input)
+        try:
+            dirs_entries.append(os.listdir(user_input))         #try to generate entry list, will fail if path can't be found
+        except FileNotFoundError:
+            logging.error(f"Directory with path \"{user_input}\" could not be found.")
+            continue                                            #try again
+        dirs_path.append(os.path.abspath(user_input))           #append absolute path
+        logging.info(f"Found directory at: \"{dirs_path[-1]}\"")
+        
+        dirs_uniques.append(copy.deepcopy(dirs_entries[-1]))    #initialise uniques with deep copy of all entries
+        i += 1
+    
+    result += "--------------------------------------------------\n"
+    for i, dir_entries in enumerate(dirs_entries):              #for all directories
+        for j, dir_entries in enumerate(dirs_entries):          #go through all other directories
+            if i==j:                                            #skip same directory
+                continue
+            for dir_entry in dir_entries:                       #and remove their entries in own unique list
+                try:
+                    dirs_uniques[i].remove(dir_entry)
+                except ValueError:
+                    pass
+        
+        result += f"Directory \"{dirs_path[i]}\" uniques:\n"    #generate result for this directory
+        if len(dirs_uniques[i])==0:                             #placeholder
+            result += "-"
+        else:
+            result += "\n".join(dirs_uniques[i])                #uniques
+        result += "\n--------------------------------------------------\n"
+    logging.info(f"{sum([len(dir_uniques) for dir_uniques in dirs_uniques])} deviations found.")
+    logging.info(result)
+    
+    logging.info(f"Writing results into file \"{RESULT_FILENAME}\"...")
+    with open(RESULT_FILENAME, "wt", encoding="utf-8") as result_file:
+        result_file.write(result)
+    logging.info(f"\rWrote results into file \"{RESULT_FILENAME}\".")
+    
+    return
